ff6a7346c30d753ba5bad863edb54d98b80b88f3
becky <satoru-kibe@rhythmcan.com>
2025-12-07 06:26:36 +0000

    langfuse_test

---
+++ chapter7/1_langfuse_trial.py
@@ -0,0 +1,22 @@
+from langchain.chat_models import init_chat_model
+from langfuse.langchain import CallbackHandler
+
+from dotenv import load_dotenv
+load_dotenv()
+
+# モデルの準備
+llm = init_chat_model(
+    model="us.anthropic.claude-3-7-sonnet-20250219-v1:0",
+    model_provider="bedrock_converse",
+)
+
+# Langfuseのコールバッククラスのインスタンスを作成
+langfuse_handler = CallbackHandler()
+config={"callbacks": [langfuse_handler]}
+
+# 推論を実行
+response = llm.invoke("こんにちは", config=config)
+print(response)
+
+
+    
